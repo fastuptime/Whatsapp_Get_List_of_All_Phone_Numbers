162c45e8afa4c7431215ad35fc56e66a442cf679
Can <63351166+fastuptime@users.noreply.github.com>
2023-05-21 20:43:11 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+
+const client = new Client({
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  authStrategy: new LocalAuth(),
+});
+
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Sistem Aktif!\nTüm Kisiler Listeleniyor...\n");
+  client.getContacts().then((contacts) => {
+    contacts.forEach((contact) => {
+      console.log(`[${contact.name}] ${contact.number}`);
+    });
+  });
+});
+
+client.initialize();
